cf1f997b33acdd81c53657523790362ce2ea5f2d
appleRui <apple31581@gmail.com>
2022-07-11 09:41:31 +0900

    1-5

---
+++ front/src/components/Basic.tsx
@@ -0,0 +1,85 @@
+/*
+基本情報の入力を管理
+*/
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { 
+  TextField,
+  FormControl,
+  FormLabel,
+  RadioGroup,
+  FormControlLabel,
+  Radio
+} from "@material-ui/core";
+import useStyles from "./styles";
+
+import { RootState } from "../domain/entity/rootState";
+import { Profile } from "../domain/entity/profile";
+import { Gender } from "../domain/entity/gender";
+
+import { PROFILE } from "../domain/services/profile";
+
+import profileActions from "../store/profile/actions";
+
+const Basic = () => {
+  const dispatch = useDispatch();
+  const profile = useSelector((state: RootState) => state.profile)
+  const classes = useStyles();
+
+  const handleChange = (member: Partial<Profile>) => {
+    dispatch(profileActions.setProfile(member));
+  };
+
+  return (
+    <>
+      <TextField 
+        fullWidth
+        className={classes.formField} 
+        label={PROFILE.NAME}
+        value={profile.name}
+        onChange={e => handleChange({ name: e.target.value })}
+      />
+      <TextField
+        fullWidth
+        multiline
+        className={classes.formField}
+        rows={5}
+        label={PROFILE.DESCRIPTION}
+        value={profile.description}
+        onChange={e => handleChange({ description: e.target.value })}
+      />
+      <FormControl className={classes.formField}>
+        <FormLabel>{PROFILE.GENDER}</FormLabel>
+        <RadioGroup
+          value={profile.gender}
+          onChange={e => handleChange({ gender: e.target.value as Gender })}
+        >
+          <FormControlLabel
+            value="male"
+            label="男性"
+            control={<Radio color="primary" />}
+          />
+          <FormControlLabel
+            value="female"
+            label="女性"
+            control={<Radio color="primary" />}
+          />
+        </RadioGroup>
+      </FormControl>
+      <TextField
+        fullWidth
+        className={classes.formField}
+        label={PROFILE.BIRTHDAY}
+        type="date"
+        value={profile.birthday}
+        InputLabelProps={{
+          shrink: true
+        }}
+        onChange={e => handleChange({ birthday: e.target.value })}
+      />
+    </>
+  );
+};
+
+export default Basic;
---
+++ front/src/components/Profile.tsx
@@ -0,0 +1,29 @@
+/* 
+フォーム全体のレイアウトや保存の処理
+*/
+
+import React from "react";
+import { Container, Typography } from "@material-ui/core";
+
+import Basic from "./Basic";
+import useStyles from "./styles";
+
+const Profile = () => {
+  const classes = useStyles();
+
+  return (
+    <Container maxWidth="sm">
+      <Typography
+        variant="h4"
+        component="h2"
+        className={classes.title}
+        color="primary"
+      >
+        基本情報
+      </Typography>
+      <Basic />
+    </Container>
+  );
+};
+
+export default Profile;
